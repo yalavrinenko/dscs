c280b1536cb33f20619a36eaf6f1424c83f48244
yalavrinenko <y.lavrinenko@neurodatalab.com>
2019-08-21 17:19:06 +0300

    add engine classes

---
+++ src/ships/engine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yalavrinenko on 21.08.2019.
+//
+
+#include "engine.hpp"
---
+++ src/ships/engine.hpp
@@ -0,0 +1,8 @@
+//
+// Created by yalavrinenko on 21.08.2019.
+//
+
+#ifndef DSCS_ENGINE_HPP
+#define DSCS_ENGINE_HPP
+
+#endif // DSCS_ENGINE_HPP
